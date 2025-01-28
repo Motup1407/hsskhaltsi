b37d991b7405db616d63302249b0adcfaee8fde5
Motup1407 <motupnang@gmail.com>
2025-01-28 15:58:21 +0530

    Updates

---
+++ assets/rewind-frplILSY.js
@@ -0,0 +1,21 @@
+import{c as o}from"./createLucideIcon-ClO3V9Og.js";/**
+ * @license lucide-react v0.471.1 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["polygon",{points:"13 19 22 12 13 5 13 19",key:"587y9g"}],["polygon",{points:"2 19 11 12 2 5 2 19",key:"3pweh0"}]],i=o("FastForward",n);/**
+ * @license lucide-react v0.471.1 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["rect",{x:"14",y:"4",width:"4",height:"16",rx:"1",key:"zuxfzm"}],["rect",{x:"6",y:"4",width:"4",height:"16",rx:"1",key:"1okwgv"}]],y=o("Pause",e);/**
+ * @license lucide-react v0.471.1 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["polygon",{points:"6 3 20 12 6 21 6 3",key:"1oa8hb"}]],a=o("Play",t);/**
+ * @license lucide-react v0.471.1 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["polygon",{points:"11 19 2 12 11 5 11 19",key:"14yba5"}],["polygon",{points:"22 19 13 12 22 5 22 19",key:"1pi1cj"}]],p=o("Rewind",s);export{i as F,a as P,p as R,y as a};
